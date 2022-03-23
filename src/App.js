754f4680472cfe456630ba1cbb5b30625217f0e0
Johnny <idsopy@nate.com>
2022-03-23 09:24:53 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./Detail.scss";
+import { useState } from "react";
 
 function App() {
+  let [game, gameChange] = useState(
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Nav">
+        <h1 className="title">Tic Tac Toe</h1>
+
+        <div className="game-container">
+          <div className="game-box">
+            <div className="row1">
+              <div
+                className="cell1"
+                onClick={() => {
+                  console.log(1);
+                }}
+              >
+                {}
+              </div>
+              <div
+                className="cell2"
+                onClick={() => {
+                  console.log(1);
+                }}
+              ></div>
+              <div
+                className="cell3"
+                onClick={() => {
+                  console.log(1);
+                }}
+              ></div>
+            </div>
+            <div className="row2">
+              <div
+                className="cell4"
+                onClick={() => {
+                  console.log(1);
+                }}
+              ></div>
+              <div
+                className="cell5"
+                onClick={() => {
+                  console.log(1);
+                }}
+              ></div>
+              <div
+                className="cell6"
+                onClick={() => {
+                  console.log(1);
+                }}
+              ></div>
+            </div>
+            <div className="row3">
+              <div
+                className="cell7"
+                onClick={() => {
+                  console.log(1);
+                }}
+              ></div>
+              <div
+                className="cell8"
+                onClick={() => {
+                  console.log(1);
+                }}
+              ></div>
+              <div
+                className="cell9"
+                onClick={() => {
+                  console.log(1);
+                }}
+              ></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// 플레이어 정하기.
+// 플레이어1 턴에는 플레이어2가 클릭 금지. ( 클릭시 O 만 )
