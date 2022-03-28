26fd6543011f060020396d77570f927b2f1d2676
Johnny <idsopy@nate.com>
2022-03-28 17:28:01 +0900

    change

--- src/App.js
+++ src/App.js
@@ -15,76 +15,81 @@ function App() {
         <h1 className="title">Tic Tac Toe</h1>
 
         <div className="game-container">
-          <div className="game-box">
-            <div className="row1">
-              <div
-                className="cell1"
-                onClick={() => {
-                  console.log(1);
-                }}
-              >
-                {}
-              </div>
-              <div
-                className="cell2"
-                onClick={() => {
-                  console.log(2);
-                }}
-              ></div>
-              <div
-                className="cell3"
-                onClick={() => {
-                  console.log(3);
-                }}
-              ></div>
-            </div>
-            <div className="row2">
-              <div
-                className="cell4"
-                onClick={() => {
-                  console.log(4);
-                }}
-              ></div>
-              <div
-                className="cell5"
-                onClick={() => {
-                  console.log(5);
-                }}
-              ></div>
-              <div
-                className="cell6"
-                onClick={() => {
-                  console.log(6);
-                }}
-              ></div>
-            </div>
-            <div className="row3">
-              <div
-                className="cell7"
-                onClick={() => {
-                  console.log(7);
-                }}
-              ></div>
-              <div
-                className="cell8"
-                onClick={() => {
-                  console.log(8);
-                }}
-              ></div>
-              <div
-                className="cell9"
-                onClick={() => {
-                  console.log(9);
-                }}
-              ></div>
-            </div>
-          </div>
+          <GameBox />
         </div>
       </div>
     </div>
   );
 }
 
+function GameBox() {
+  return (
+    <div className="game-box">
+      <div className="row1">
+        <div
+          className="cell1"
+          onClick={() => {
+            console.log(1);
+          }}
+        >
+          {}
+        </div>
+        <div
+          className="cell2"
+          onClick={() => {
+            console.log(2);
+          }}
+        ></div>
+        <div
+          className="cell3"
+          onClick={() => {
+            console.log(3);
+          }}
+        ></div>
+      </div>
+      <div className="row2">
+        <div
+          className="cell4"
+          onClick={() => {
+            console.log(4);
+          }}
+        ></div>
+        <div
+          className="cell5"
+          onClick={() => {
+            console.log(5);
+          }}
+        ></div>
+        <div
+          className="cell6"
+          onClick={() => {
+            console.log(6);
+          }}
+        ></div>
+      </div>
+      <div className="row3">
+        <div
+          className="cell7"
+          onClick={() => {
+            console.log(7);
+          }}
+        ></div>
+        <div
+          className="cell8"
+          onClick={() => {
+            console.log(8);
+          }}
+        ></div>
+        <div
+          className="cell9"
+          onClick={() => {
+            console.log(9);
+          }}
+        ></div>
+      </div>
+    </div>
+  );
+}
 export default App;
 
 // 플레이어 정하기.
