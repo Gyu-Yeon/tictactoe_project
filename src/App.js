4591e42d6e79b769d0578116b78528f4a6a0a69f
Johnny Choi <idsopy@nate.com>
2022-05-27 02:20:55 +0900

    change

--- src/App.js
+++ src/App.js
@@ -4,19 +4,30 @@ import "./Detail.scss";
 import { useState } from "react";
 
 function App() {
-  let [turn, turnChange] = useState(0);
-  let [game, gameChange] = useState([
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
+  let [turn, setTurn] = useState(0);
+  let [mark, setMark] = useState([
+    "", "", "",
+    "", "", "",
+    "", "", "",
   ]);
+
+// const turnChange = ()=>{
+//   if(turn === 0){
+//     setMark((mark[] = "X"))
+//     setTurn(1)
+//   }else if(turn === 1){
+//     setMark((mark[] = "O"))
+//     setTurn(0)
+//   }
+// }
+
   return (
     <div className="App">
       <div className="Nav">
         <h1 className="title">Tic Tac Toe</h1>
 
         <div className="game-container">
-          <GameBox game={game} gameChange={gameChange} />
+          <GameBox mark={mark} turnChange={turnChange} />
         </div>
       </div>
     </div>
@@ -24,69 +35,68 @@ function App() {
 }
 
 function GameBox(props) {
-  return (
+  return (   
     <div className="game-box">
       <div className="row1">
         <div
           className="cell1"
-          onClick={() => {
-            props.gameChange((props.game[0][0] = "x"));
-          }}
-        >
-          {props.game[0][0]}
+          data-id="0"
+          onClick={() => console.log(e.target.id)}> 
+        <span className="marks">{props.mark[0]}</span>        
         </div>
         <div
           className="cell2"
           onClick={() => {
-            console.log(2);
-          }}
-        ></div>
+          props.turnChange()}}>
+          <span className="marks">{props.mark[0][1]}</span>     
+        </div>
         <div
           className="cell3"
           onClick={() => {
-            console.log(3);
-          }}
-        ></div>
+          props.turnChange()}}>
+          <span className="marks">{props.mark[0][2]}</span>   
+        </div>
       </div>
       <div className="row2">
         <div
           className="cell4"
           onClick={() => {
-            console.log(4);
-          }}
-        ></div>
+          props.turnChange()}}>
+        <span className="marks">{props.mark[1][0]}</span>       
+        </div>
         <div
           className="cell5"
           onClick={() => {
-            console.log(5);
-          }}
-        ></div>
+          props.turnChange()
+          }}>
+        <span className="marks">{props.mark[1][1]}</span>       
+        </div>
         <div
           className="cell6"
           onClick={() => {
-            console.log(6);
-          }}
-        ></div>
+          props.turnChange()}}>
+        <span className="marks">{props.mark[1][2]}</span>         
+        </div>
       </div>
       <div className="row3">
         <div
           className="cell7"
           onClick={() => {
-            console.log(7);
-          }}
-        ></div>
+          props.turnChange()}}>
+        <span className="marks">{props.mark[2][0]}</span>   
+        </div>
         <div
           className="cell8"
           onClick={() => {
-            console.log(8);
-          }}
-        ></div>
+            props.turnChange()}}>
+        <span className="marks">{props.mark[2][1]}</span>       
+        </div>
         <div
           className="cell9"
           onClick={() => {
-            console.log(9);
-          }}
-        ></div>
+          props.turnChange()}}>
+        <span className="marks">{props.mark[0][0]}</span>     
+        </div>
       </div>
     </div>
   );
